209a871915ecf4a18f2bdaa191802bde2a63fca8
Anmol sharma <as7067647@gmail.com>
2025-02-07 19:33:56 +0530

    add project title and add attribute in anchor tag like target

--- script.js
+++ script.js
@@ -4,33 +4,30 @@ function onToggleMenu(e) {
   navLinks.classList.toggle("top-[55px]");
 }
 
-
 const certificate = [
-  './images/certificate2.png',
-  './images/certificate3.png',
-  './images/certificate4.png',
-  './images/certificate5.png',
-  './images/certificate6.png',
-  './images/certificate7.png',
-  './images/certificate8.png',
-  './images/certificate9.png',
-  './images/certificate10.png',
-  './images/certificate11.png',
-  './images/certificate12.png'
-]
+  "./images/certificate2.png",
+  "./images/certificate3.png",
+  "./images/certificate4.png",
+  "./images/certificate5.png",
+  "./images/certificate6.png",
+  "./images/certificate7.png",
+  "./images/certificate8.png",
+  "./images/certificate9.png",
+  "./images/certificate10.png",
+  "./images/certificate11.png",
+  "./images/certificate12.png",
+];
 
 let index = 0;
-const certificateElements = document.querySelector('#certificates');
+const certificateElements = document.querySelector("#certificates");
 
-setInterval(()=>{
-certificateElements.style.opacity = '0';
+setInterval(() => {
+  certificateElements.style.opacity = "0";
 
-setTimeout(()=>{
-index = (index + 1) % certificate.length;
-certificateElements.src = certificate[index];
+  setTimeout(() => {
+    index = (index + 1) % certificate.length;
+    certificateElements.src = certificate[index];
 
-certificateElements.style.opacity = '1';
-
-},1000)
-
-},4000)
+    certificateElements.style.opacity = "1";
+  }, 1000);
+}, 4000);
